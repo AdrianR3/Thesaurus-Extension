ea43ffccd3545f999bf4d430fe70bf87c10dd930
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2024-08-14 13:42:37 -0700

    Fix Horizontal Scroll, Add Icon

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,1 +1,31 @@
 console.log("popup.js | Hello world!")
+
+// document.getElementById('go').onsubmit = function() {
+//     console.log("GO")
+//     return false;
+// };
+
+var form = document.getElementById('searchForm');
+if (form.attachEvent) {
+    form.attachEvent("submit", processForm);
+    form.attachEvent("keyup", keyEvent);
+} else {
+    form.addEventListener("submit", processForm);
+    form.addEventListener("keyup", keyEvent);
+}
+
+function processForm(e) {
+    if (e.preventDefault) e.preventDefault();
+
+    console.log("GO")
+
+    // Also to prevent default
+    return false;
+}
+
+function keyEvent(e) {
+    if (e.keyCode == 9) {
+        console.log("Tab key pressed")
+        e.preventDefault();
+    }
+}
