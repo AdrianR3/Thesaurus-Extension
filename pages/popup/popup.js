84c8a20fe1185e5d5e54d20f684c9bda2eec2c91
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2024-08-17 20:36:34 -0700

    Add Context Action

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -1,11 +1,28 @@
 console.log("popup.js | Hello world!")
 
-// document.getElementById('go').onsubmit = function() {
-//     console.log("GO")
-//     return false;
-// };
+const queryParameters = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
 
-var form = document.getElementById('searchForm');
+if (queryParameters.word) {
+    document.getElementById('search').value = queryParameters.word;
+    processForm();
+} else {
+    chrome.storage.local.get('word', function(result) {
+        if (!result.word) return;
+        document.getElementById('search').value = result.word;
+        processForm();
+    });
+    chrome.storage.local.remove('word', function() {
+        if (chrome.runtime.lastError) {
+            console.error('Error removing word for storage:', chrome.runtime.lastError);
+        }
+    });
+}
+
+console.log(`Word: ${document.getElementById('search').value}`)
+
+let form = document.getElementById('searchForm');
 if (form.attachEvent) {
     form.attachEvent("submit", processForm);
     form.attachEvent("keyup", keyEvent);
@@ -15,7 +32,7 @@ if (form.attachEvent) {
 }
 
 function processForm(e) {
-    if (e.preventDefault) e.preventDefault();
+    if (e && e.preventDefault) e.preventDefault();
 
     const word = document.getElementById('search').value;
     document.activeElement.blur();
@@ -28,14 +45,16 @@ function processForm(e) {
         .then(response => response.json())
         .then(response => {
             const synonyms = findValuesByKey(response, "synonyms");
-            console.log(synonyms);
             changeContent(synonyms)
 
+            if (synonyms.length == 0) {
+                document.querySelector('html').style.height = "200px"
+            } else {
+                document.querySelector('html').style.height = "600px"
+            }
             document.getElementById('go').style.animation="idle";
         })
         .catch(err => console.error(err));
-
-    console.log(word)
 
     // Also to prevent default
     return false;
@@ -45,12 +64,21 @@ function changeContent(synonyms) {
     const synonymList = document.getElementById('synonymList');
 
     synonymList.innerHTML = '';
-
-    synonyms.forEach(synonym => {
+    
+    if (synonyms.length == 0) {
         const item = document.createElement('li');
-        item.textContent = synonym;
+        item.classList.add('synonym', 'none-found')
+        item.textContent = "No Results Found"
         synonymList.appendChild(item);
-    });
+    } else {
+        synonyms.forEach(synonym => {
+            const item = document.createElement('li');
+            item.classList.add('synonym')
+            item.textContent = synonym;
+            synonymList.appendChild(item);
+        });
+    }
+    
 }
 
 function findValuesByKey(obj, targetKey) {
--- scripts/background.js
+++ scripts/background.js
@@ -1,1 +1,32 @@
 console.log("background.js | Hello world!")
+
+chrome.contextMenus.create({
+    id: "findSynonym",
+    title: "Find synonyms for '%s'",
+    contexts: ["selection"]
+});
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "findSynonym") {
+
+        // console.log(info.selectionText)
+        chrome.scripting.executeScript({
+            target: { tabId: tab.id },
+            function: handleSelectedText,
+            args: [info.selectionText]
+        });
+
+        chrome.storage.local.set({ word: info.selectionText });
+        chrome.action.openPopup()
+        
+        // chrome.tabs.create({ 
+        //     url: `${chrome.runtime.getURL("pages/popup/popup.html")}?word=${info.selectionText}` 
+        // });
+        
+    }
+});
+
+// Injected Function
+function handleSelectedText(selectedText) {
+    console.log(`Finding synonym for ${selectedText}`)
+}
