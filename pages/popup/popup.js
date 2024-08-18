be536944b7c82e4567e598614dce59cbd6eaa8ba
AdrianR3 <93000948+AdrianR3@users.noreply.github.com>
2024-08-17 19:28:56 -0700

    Add basic fetch

--- pages/popup/popup.js
+++ pages/popup/popup.js
@@ -17,10 +17,63 @@ if (form.attachEvent) {
 function processForm(e) {
     if (e.preventDefault) e.preventDefault();
 
-    console.log("GO")
+    const word = document.getElementById('search').value;
+    document.activeElement.blur();
+
+    document.getElementById('go').style.animation="spin1 4s linear infinite";
+
+    const options = {method: 'GET', headers: {'User-Agent': 'insomnia/9.3.3'}};
+
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`, options)
+        .then(response => response.json())
+        .then(response => {
+            const synonyms = findValuesByKey(response, "synonyms");
+            console.log(synonyms);
+            changeContent(synonyms)
+
+            document.getElementById('go').style.animation="idle";
+        })
+        .catch(err => console.error(err));
+
+    console.log(word)
 
     // Also to prevent default
     return false;
+}
+
+function changeContent(synonyms) {
+    const synonymList = document.getElementById('synonymList');
+
+    synonymList.innerHTML = '';
+
+    synonyms.forEach(synonym => {
+        const item = document.createElement('li');
+        item.textContent = synonym;
+        synonymList.appendChild(item);
+    });
+}
+
+function findValuesByKey(obj, targetKey) {
+    let results = [];
+
+    function search(obj) {
+        if (typeof obj !== 'object' || obj === null) return;
+
+        // Check if the current level has the target key
+        if (obj.hasOwnProperty(targetKey) && obj[targetKey].length != 0) {
+            results.push(...obj[targetKey]);
+        }
+
+        // Recursively search in all properties
+        for (let key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                search(obj[key]);
+            }
+        }
+    }
+
+    search(obj);
+    return results;
 }
 
 function keyEvent(e) {
